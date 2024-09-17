73aaf251291515eb5c301dae8026474fe6a99253
CarolineHickey <caroline@smarty.com>
2024-09-17 10:23:45 -0600

    Additional calculation features

--- calc/calc.go
+++ calc/calc.go
@@ -5,3 +5,27 @@ type Addition struct{}
 func (this Addition) Calculate(a, b int) int {
 	return a + b
 }
+
+type Subtraction struct{}
+
+func (this Subtraction) Calculate(a, b int) int {
+	return a - b
+}
+
+type Division struct{}
+
+func (this Division) Calculate(a, b int) int {
+	return a / b
+}
+
+type Multiplication struct{}
+
+func (this Multiplication) Calculate(a, b int) int {
+	return a * b
+}
+
+type Exponential struct{}
+
+func (this Exponential) Calculate(a, b int) int {
+	return a ^ b
+}
